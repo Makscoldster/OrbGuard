4e8a777f67b2113cab97be6acae1e4392339c566
Makscold <makscoldster773@gmail.com>
2026-03-18 14:51:27 +0200

    Створено GameObject & Orb

---
+++ OrbGuard/Entities/GameObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media;
+
+namespace OrbGuard.Entities
+{
+    public abstract class GameObject
+    {
+        // Позиція і розмір
+        public double X { get; protected set; }
+        public double Y { get; protected set; }
+        public double Width { get; protected set; }
+        public double Height { get; protected set; }
+        public bool IsAlive { get; protected set; } = true;
+
+        protected GameObject(double x, double y, double width, double height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+
+        public abstract void Update(double deltaTime);
+        public abstract void Render(DrawingContext dc);
+
+        public virtual void Destroy()
+        {
+            IsAlive = false;
+        }
+    }
+}
---
+++ OrbGuard/Entities/Orb.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace OrbGuard.Entities
+{
+    public class Orb : GameObject
+    {
+        public double MaxHp { get; private set; }
+        public double CurrentHp { get; private set; }
+        public double RegenRate { get; private set; } // HP за секунду
+
+        private readonly Brush _normalBrush = Brushes.DeepSkyBlue;
+        private readonly Brush _damagedBrush = Brushes.OrangeRed;
+
+        public bool IsDestroyed => CurrentHp <= 0;
+
+        public Orb(double x, double y) : base(x, y, 40, 40)
+        {
+            MaxHp = 100;
+            CurrentHp = 100;
+            RegenRate = 2.0;
+        }
+
+        public void TakeDamage(double amount)
+        {
+            CurrentHp = Math.Max(0, CurrentHp - amount);
+            if (CurrentHp <= 0)
+                Destroy();
+        }
+
+        public void Regenerate(double deltaTime)
+        {
+            if (!IsAlive) return;
+            CurrentHp = Math.Min(MaxHp, CurrentHp + RegenRate * deltaTime);
+        }
+
+        public override void Update(double deltaTime)
+        {
+            Regenerate(deltaTime);
+        }
+
+        public override void Render(DrawingContext dc)
+        {
+            Brush brush = CurrentHp < MaxHp * 0.3 ? _damagedBrush : _normalBrush;
+            dc.DrawEllipse(brush, null, new Point(X, Y), Width / 2, Height / 2);
+        }
+    }
+}
